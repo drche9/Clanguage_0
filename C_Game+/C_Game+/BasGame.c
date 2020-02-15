ab8f24365c62ad47998b56fd1666c0dfe8300abc
drche9 <58885721+drche9@users.noreply.github.com>
2020-02-15 23:59:22 +0900

    2.15
    31을 초과해도 계속 실행되는 버그를 수정함

--- C_Game+/C_Game+/BasGame.c
+++ C_Game+/C_Game+/BasGame.c
@@ -18,6 +18,7 @@ int main() {
 	int player = 0;
 	int computer = 0;
 	int exit0 = 0;
+	int total = 0;
 	//플레이어에게 게임 설명
 	printf("게임 설명 \n \n");
 	printf("player와 computer는 서로 숫자를 주고 받는다. \n");
@@ -25,17 +26,23 @@ int main() {
 	printf("컴퓨터는 플레이어를 마지막 값을 이어서 랜덤으로 출력한다. \n");
 	printf("31를 외치는 사람이 패배, 31을 외치지 않는 사람이 승리한다. \n");
 	printf("start! \n");
-	//반복
-	while (player || computer <= 31) {
-		//플레이어의 숫자 입력
-		printf("플레이어 : ");
+	//반복(31까지)
+	while (total < 31)
+	{
+		//플레이어의 입력
+		printf("player : ");
 		scanf_s("%d", &player);
-		//컴퓨터의 랜덤값 입력
+		//컴퓨터의 입력
 		srand(time(NULL));
-		computer = (rand() % 3) + 1;
-		printf("\n컴퓨터 : %d \n \n",player + computer);
+		if (player < 31) {
+			computer = (rand() % 3) + 1;
+		}
+		total = computer + player;
+		printf("computer : %d \n \n", total);
 	}
-	printf("END");
+	printf("GameOver");
 	//31에 다다르면 게임 오버
+	
+	
 	return 0;
 }
